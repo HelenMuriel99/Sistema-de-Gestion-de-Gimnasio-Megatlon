b125de6143392b7f22b0813fff5f2b8dc05259a3
xXCaptainJhonaXx <fabrizzioachabal070601@gmail.com>
2026-08-09 02:27:26 -0400

    feat: Inicializacion de backend SpringBoot con perfiles dev, qa, prod. Creacion de la base de datos en SQLite.

---
+++ src/main/java/com/backend/megatlon/MegatlonApplication.java
@@ -0,0 +1,13 @@
+package com.backend.megatlon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegatlonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegatlonApplication.class, args);
+	}
+
+}
